b93a00e998f108756f2336cac445f192eca1c525
Juan Melendres <juan.mele97@gmail.com>
2025-02-27 11:48:18 -0600

    Enhance UI with Tailwind CSS styling for header and body in Calorie Tracker

--- calorie-tracker/src/App.tsx
+++ calorie-tracker/src/App.tsx
@@ -2,7 +2,20 @@ function App() {
 
   return (
     <>
-      <h1>Calorie Tracker</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Calorie Tracker
+          </h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+    
+        </div>
+      </section>
+
     </>
   )
 }
